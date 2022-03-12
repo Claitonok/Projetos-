f6564e1e76d287a7a9b48fcffb88f72bcbae8350
Claitonok <101401437+Claitonok@users.noreply.github.com>
2022-03-12 09:44:18 -0300

    Projetos
    construindo class

---
+++ Main (1).java
@@ -0,0 +1,15 @@
+public class Main
+{
+	public static void main(String[] args) {
+	 
+	 Pessoa pessoa1 = new Pessoa();
+	 Pessoa.nome = "Pedro Almeida";
+	 Pessoa.altura = 1.83;
+	 pessoa1.peso = 85;
+	 
+	 
+	
+	
+	
+	}
+}
